78b6459d47757dc0b72e9769e19167b5a816f6f3
P156HAM <a.hamdi.almasalmeh@gmail.com>
2024-02-02 19:18:54 +0100

    feat: Get prompt, prompt feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,9 +2,31 @@
 import React, { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  );
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [posts, setposts] = useState([]);
   const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setposts(data);
+    };
+
+    fetchPost();
+  }, []);
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -17,6 +39,8 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
